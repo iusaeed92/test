06b5d8323b08a4a288d15b324e7ed2144c571b66
Ibrahim Urooj Saeed <ibrahim.urooj@gmail.com>
2014-11-17 01:46:59 -0500

    Okay, more changes.
    Adding some files.

---
+++ Cats/Cats/Chill.cpp
@@ -0,0 +1,9 @@
+//
+//  Chill.cpp
+//  Cats
+//
+//  Created by Ibrahim Saeed on 11/17/14.
+//  Copyright (c) 2014 Ibrahim Saeed. All rights reserved.
+//
+
+#include "Chill.h"
---
+++ Cats/Cats/Chill.h
@@ -0,0 +1,24 @@
+//
+//  Chill.h
+//  Cats
+//
+//  Created by Ibrahim Saeed on 11/17/14.
+//  Copyright (c) 2014 Ibrahim Saeed. All rights reserved.
+//
+
+#ifndef __Cats__Chill__
+#define __Cats__Chill__
+
+#include <stdio.h>
+
+
+class chill {
+    public :
+    
+    chill();
+    chill (int x, char y);
+
+    
+};
+
+#endif /* defined(__Cats__Chill__) */
